b3883d18f2c5a2dfa667790207eb7f2b4b7acdd9
Adrysanisidro <adrys@DESKTOP-MU3GF74>
2018-04-13 13:00:50 +0200

    Formated

--- src/simulacion_mensario/Hilo.java
+++ src/simulacion_mensario/Hilo.java
@@ -5,7 +5,10 @@
  */
 package simulacion_mensario;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import static simulacion_mensario.Envios.*;
+
 /**
  *
  * @author adrys
@@ -17,11 +20,12 @@ public class Hilo extends Thread {
 
     public void run() {
         while (x == 2) {
-            comprobarTelefonos();
             validarTelefonos();
             lblNumCaracteres.setText("" + txtAreaMensaje.getText().length());
             comprobarCheck();
+            lblMovilesInsertados.setText(comprobarTelefonos());
             comprobarNumMensajes();
+           
         }
     }
 
@@ -35,38 +39,48 @@ public class Hilo extends Thread {
         }
     }
 
-    public void comprobarTelefonos() {
+    public static String comprobarTelefonos() {
         String[] contador = txtAreaMoviles.getText().split(",");
+        String cantMoviles="0";
         if (contador.length > 1) {
-            lblMovilesInsertados.setText("" + contador.length);
+            //lblMovilesInsertados.setText("" + contador.length);
+            cantMoviles=""+contador.length;
         } else {
             if (!"".equals(txtAreaMoviles.getText())) {
-                lblMovilesInsertados.setText("1");
+                //lblMovilesInsertados.setText("1");
+                cantMoviles="1";
             } else {
-                lblMovilesInsertados.setText("0");
+                //lblMovilesInsertados.setText("0");
+                cantMoviles="0";
             }
         }
-        
+        return cantMoviles;
     }
-    public void validarTelefonos(){
+
+    public void validarTelefonos() {
         String[] contador = txtAreaMoviles.getText().split(",");
-        Boolean[] comprobador=new Boolean[contador.length];
-        for(int i=0;i<contador.length;i++){
-            if((contador[i].startsWith("6")||contador[i].startsWith("7"))&&contador[i].length()==9){
-                comprobador[i]=true;
-            }else{
-                comprobador[i]=false;
+        Boolean[] comprobador = new Boolean[contador.length];
+        for (int i = 0; i < contador.length; i++) {
+            if ((contador[i].startsWith("6") || contador[i].startsWith("7")) && contador[i].length() == 9) {
+                comprobador[i] = true;
+            } else {
+                comprobador[i] = false;
             }
         }
-        if(comprobarBooleans(comprobador)==true){
+        if (comprobarBooleans(comprobador) == true) {
             btnEnviar.setEnabled(true);
-        }else{
+        } else {
             btnEnviar.setEnabled(false);
         }
     }
-    public boolean comprobarBooleans(Boolean[] array){
-    for(boolean b : array) if(!b) return false;
-    return true;
-}
-    
+
+    public boolean comprobarBooleans(Boolean[] array) {
+        for (boolean b : array) {
+            if (!b) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
