368e0ea1936ba2d91aa18aa8e978411bdd90c895
Adrysanisidro <adrys@DESKTOP-MU3GF74>
2018-04-17 15:55:38 +0200

    Añadida la api de mensario a la carpeta y quitada la base de datos

--- src/simulacion_mensario/Simulacion_Mensario.java
+++ src/simulacion_mensario/Simulacion_Mensario.java
@@ -14,7 +14,7 @@ public class Simulacion_Mensario {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Envios objEnv=new Envios();
         objEnv.setVisible(true);
     }
